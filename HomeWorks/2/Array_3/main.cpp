1fcc8ede5e8d378c38462efc5f481b92425203d1
tihonov-e <tom-step@ya.ru>
2016-10-17 21:23:08 +0300

    create 2_web

---
+++ HomeWorks/2/Array_3/main.cpp
@@ -0,0 +1,11 @@
+/*”далить в массиве все числа, которые повтор€ютс€ более двух раз.
+   »сточник: http://www.itmathrepetitor.ru/s-zadachi-s-resheniyami-odnomernye-mass/*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
